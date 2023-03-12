eed6e964a202e46e50ae1e972734bd0d0e484c0e
anizmo <potdar.anuj@gmail.com>
2023-03-12 18:46:57 -0400

    Linear Layout example done, commenting left

--- app/src/main/java/edu/neu/madcourse/viewgroups/LinearLayoutActivity.java
+++ app/src/main/java/edu/neu/madcourse/viewgroups/LinearLayoutActivity.java
@@ -3,12 +3,65 @@ package edu.neu.madcourse.viewgroups;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class LinearLayoutActivity extends AppCompatActivity {
+
+    private View redBlock;
+    private View greenBlock;
+    private View blueBlock;
+
+    private CheckBox redCheckBox;
+    private CheckBox greenCheckBox;
+    private CheckBox blueCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout);
+
+        redBlock = findViewById(R.id.redBlock);
+        greenBlock = findViewById(R.id.greenBlock);
+        blueBlock = findViewById(R.id.blueBlock);
+
+        redCheckBox = findViewById(R.id.redCheckBox);
+        greenCheckBox = findViewById(R.id.greenCheckBox);
+        blueCheckBox = findViewById(R.id.blueCheckBox);
+
+        redCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                if (checked) {
+                    redBlock.setVisibility(View.VISIBLE);
+                } else {
+                    redBlock.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        greenCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                if (checked) {
+                    greenBlock.setVisibility(View.VISIBLE);
+                } else {
+                    greenBlock.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        blueCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                if (checked) {
+                    blueBlock.setVisibility(View.VISIBLE);
+                } else {
+                    blueBlock.setVisibility(View.GONE);
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/edu/neu/madcourse/viewgroups/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/viewgroups/MainActivity.java
@@ -1,13 +1,29 @@
 package edu.neu.madcourse.viewgroups;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button linearLayoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearLayoutButton = findViewById(R.id.linearLayout);
+
+        linearLayoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LinearLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
